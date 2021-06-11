e9ed25b41bac6dfb9fd54aee212d061ac0a03925
Carolyn Gregoris <carolyn.a.scobie@gmail.com>
2021-06-11 17:55:44 -0600

    commit

--- backend/db/database.sql
+++ backend/db/database.sql
@@ -29,7 +29,9 @@ CREATE TABLE transactions (
   category_id INTEGER REFERENCES category(id) ON DELETE CASCADE,
   description VARCHAR(255),
   date VARCHAR(255),
-  amount DECIMAL(12,2)
+  amount DECIMAL(12,2),
+  month VARCHAR(255),
+  year INTEGER
 );
 
 CREATE TABLE badges (
