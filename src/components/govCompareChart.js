b52d6b9df8627874f3b167e4cf3f43794487f73b
Kiup Kim <kukim96@gmail.com>
2021-06-14 11:32:14 -0600

    changed color on insights page and also changed info on flipcards

--- src/components/govCompareChart.js
+++ src/components/govCompareChart.js
@@ -54,12 +54,12 @@ const GovCompareChart = () => {
       {
         label: 'average Canadian expenses (%)',
         data: ['3.45', '4.31', '18.58', '5.34', '14.63', '7.79', '29.32', '16.58'],
-        backgroundColor: 'rgb(255, 99, 132)',
+        backgroundColor: '#FFCCBC',
       },
       {
         label: 'Your expenses (%)',
         data: expensesRatio(),
-        backgroundColor: 'rgb(54, 162, 235)',
+        backgroundColor: '#77A6F7',
       }
     ],
   };
--- src/components/saveflipcard.js
+++ src/components/saveflipcard.js
@@ -1,24 +1,39 @@
 import { useRef } from 'react';
 import Flippy, { FrontSide, BackSide } from 'react-flippy';
 
+import '../styles/flipcard.css'
+
 export default function SaveFlipCard() {
   const ref = useRef();
   return (
     <Flippy
       flipOnHover={false} // default false
       flipOnClick={true} // default false
-      flipDirection="vertical" // horizontal or vertical
+      flipDirection="horizontal" // horizontal or vertical
       ref={ref} // to use toggle method like ref.curret.toggle()
       // if you pass isFlipped prop component will be controlled component.
       // and other props, which will go to div
       style={{ width: '200px', height: '200px' }} /// these are optional style, it is not necessary
   >
-    <FrontSide style={{ backgroundColor: 'rgb(255, 99, 132)'}} >
-      Your Savings Rate for May-21: {((5000 - 3769.50) / 5000 * 100).toFixed(2)}% <br />
+    <FrontSide style={{ backgroundColor: '#97b1de'}} >
+      <div className="title">
+        <h3>
+          Your Savings Rate for May-21:
+        </h3>
+      </div> 
+      <div className="rate_ratio">
+      {((5000 - 3769.50) / 5000 * 100).toFixed(2)}%
+      </div>
+       <br />
       {/* <button onClick={() => { ref.current.toggle(); }}></button> */}
     </FrontSide>
-    <BackSide style={{ backgroundColor: 'rgb(54, 162, 235)'}}>
-      Savings Rate is the difference between your Income and Expenses divided by your Income.
+    <BackSide style={{ backgroundColor: '#deb497'}}>
+      <div className="backtitle">
+        Savings Rate
+      </div>
+      <div className="formula">
+        (Income - Expenses) / Income
+      </div>
       You should aim to have at least 10% Savings rate.
     </BackSide>
   </Flippy>
